cf0a33368162bef67962d975b13a439f6824b4f4
Aleksandra Shchegoleva <57931592+aleksandra-shchegoleva@users.noreply.github.com>
2025-01-06 20:24:13 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,25 +2,15 @@ from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['POST', "GET"])
 def main():
-    return render_template("main.html")
-
-@app.route("/products/<name>")
-def products(name):
-    return f"Hello {name}"
-
-@app.route('/<name>')
-def product(name):
-    if name == 'main':
-        return redirect(url_for('main'))
-    else:
-        return redirect(url_for('products', name=name))
+    if request.method == 'POST':
+        result = request.form
+        return render_template("main.html", result=result)
+    return render_template("main.html", result=[])
     
 @app.route('/form', methods=['POST', 'GET'])
 def form():
-    if request.method == "POST":
-        result = request.form
     return render_template("form.html")
 
 if __name__ == "__main__":
