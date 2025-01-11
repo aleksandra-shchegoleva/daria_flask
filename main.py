5e6bd1142a2c77cb3d8efd21326df5362fa1c075
Aleksandra Shchegoleva <57931592+aleksandra-shchegoleva@users.noreply.github.com>
2025-01-11 18:59:35 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,13 +6,25 @@ app.secret_key = 'fseffesfsfsefs'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
 @app.route('/', methods=['POST', "GET"])
 def main():
-    
+    products = Product.query.all()
+    return render_template("main.html", products=products)
     
 @app.route('/form', methods=['POST', 'GET'])
 def form():
     return render_template("form.html")
+
+with app.app_context():
+    db.create_all()
+    product = Product(name='Стул', price=1.4)
+    db.session.add(product)
+    db.session.commit()
 
 if __name__ == "__main__":
     app.run(debug=True)
