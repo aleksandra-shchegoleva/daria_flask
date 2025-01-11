18fd76ac59e4479a757b6d8792184601a040fd04
Aleksandra Shchegoleva <57931592+aleksandra-shchegoleva@users.noreply.github.com>
2025-01-11 18:32:52 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = 'fseffesfsfsefs'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
 @app.route('/', methods=['POST', "GET"])
 def main():
-    if request.method == 'POST':
-        result = request.form
-        return render_template("main.html", result=result)
-    return render_template("main.html", result=[])
+    
     
 @app.route('/form', methods=['POST', 'GET'])
 def form():
